7fb4ed74ea678fac5797c3ebc1ddd760b4c9865e
CodyPaper <45218297+CodyPaper@users.noreply.github.com>
2020-11-11 15:09:12 +0900

    JavaScript: Navbar Menu Click

--- main.js
+++ main.js
@@ -18,6 +18,9 @@
 //   }
 // });
 
+
+
+
 // Make navbar transparent when it is on the top
 
 const navbar = document.querySelector('#navbar');
@@ -31,4 +34,46 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+})
+
+// $('#navbar a, .home__contact').on('click', function(event) {
+//   if (this.hash !== '') {
+//     event.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top - 100
+//         // negative -100 means to show the each title in section
+//       },
+//       800
+//       // miler seconds
+//     );
+//   }
+// });
+
+// const menuItem = document.querySelector('.navbar__menu__items');
+// menuItem.addEventListener('click', () => {
+//   const about = document.querySelector('#about');
+//   about.scrollIntoView();
+//   const work = document.querySelector('#work');
+//   work.scrollIntoView();
+//   const contact = document.querySelector('#contact');
+//   contact.scrollIntoView();
+// })
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  // console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
 })
